7f29239cbda2fae9bd1f901e87e0abe67908c6ce
Anthony Graignic <anthony@graignic.com>
2022-06-07 17:49:18 +0200

    Init website

---
+++ website/src/lib/components/Footer.svelte
@@ -0,0 +1,45 @@
+<footer class="container m-auto p-4 bg-white sm:p-6 dark:bg-gray-800">
+	<div class="sm:flex sm:items-center sm:justify-between">
+		<span>
+			<p>
+				A project by
+				<a href="https://twitter.com/lisr_D" rel="external noopener" target="_blank">
+					punkess.eth
+				</a>
+				
+				&
+				<a href="https://twitter.com/agraignic" rel="external noopener" target="_blank">
+					agraignic.eth
+				</a>
+			</p>
+		</span>
+		<div class="flex mt-4 space-x-6 sm:justify-center sm:mt-0 text-sm">
+			<a
+				href="https://discord.gg"
+				rel="external noopener"
+				target="_blank"
+				class="text-gray-500 hover:text-gray-900 dark:hover:text-white"
+			>
+				Lens
+			</a>
+			<a
+				href="https://twitter.com/minttrees_dao"
+				rel="external noopener"
+				target="_blank"
+				class="text-gray-500 hover:text-gray-900 dark:hover:text-white"
+			>
+				Twitter
+			</a>
+		</div>
+	</div>
+</footer>
+
+<style lang="postcss">
+	span {
+		@apply text-sm text-gray-500 sm:text-center dark:text-gray-400;
+		@apply flex flex-col sm:flex-row;
+	}
+	a:hover {
+		@apply underline;
+	}
+</style>
---
+++ website/src/lib/components/PlantForm.svelte
@@ -0,0 +1,44 @@
+<script>
+	let trees;
+	let unitPrice = 0.33;
+	function onSubmit() {
+		alert(`Planted ${trees} trees`);
+	}
+</script>
+
+<form class="flex flex-col justify-items-center" on:submit={onSubmit}>
+	<div class="mb-6">
+		<label for="trees" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+			>Trees to plant</label
+		>
+		<input
+			type="number"
+			id="trees"
+			class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
+			placeholder="1000"
+			bind:value={trees}
+			required
+		/>
+	</div>
+	<div class="mb-6">
+		<label for="unitPrice" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+			>USDC per tree</label
+		>
+		<input
+			type="number"
+			id="unitPrice"
+			step=".01"
+			class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
+			bind:value={unitPrice}
+			required
+		/>
+	</div>
+
+	<p>Total: {trees ? (trees * unitPrice).toFixed(2) : '0'} USDC</p>
+
+	<button
+		type="submit"
+		class="self-end text-white bg-green-700 hover:bg-green-800 focus:outline-none focus:ring-4 focus:ring-green-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
+		>plant</button
+	>
+</form>
